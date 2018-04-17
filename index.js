de845b6d7604a1edf1c550ce8a92b275f78c2a07
Daniel Walz <walzdan@gmail.com>
2018-04-17 11:02:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key) {
-  const newObj = Object.assign({}, )
+  const newObj = Object.assign({}, object)
+  
 }
